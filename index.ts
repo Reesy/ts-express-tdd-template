a1a60e967e19068c350054175037006cc01b8bab
Reesy <James.rees@outlook.com>
2021-08-23 17:51:03 +0100

    Added an API test and abstracted out the express app to its own module so that the app can be passed into the API tests

--- index.ts
+++ index.ts
@@ -1,22 +1,7 @@
-import express from 'express';
+import { app } from './app';
 import { config } from './config';
-
-const app = express();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 
 app.listen(config.port, () =>
 {
   console.log(`Server listening on port ${config.port}`);
-});
-
-app.get('/api/v1/test1', (req: express.Request, res: express.Response) =>
-{
-  res.json({ message: 'Hello World!' });
-});
-
-app.post('/api/v1/test1', (req: express.Request, res: express.Response) =>
-{
-  res.json({ message: 'Hello World!' });
 });
